2fd57529da6b752d0cadd3a4aa8e6cbf6728cb67
Siva <sivadotpc@gmail.com>
2022-12-25 20:26:15 +0530

    lab 4 final commit

--- app/src/main/java/com/example/narendranlabassignment/MainActivity.kt
+++ app/src/main/java/com/example/narendranlabassignment/MainActivity.kt
@@ -7,9 +7,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.Modifier
-import com.example.narendranlabassignment.ui.theme.*
-import com.ramcosta.composedestinations.DestinationsNavHost
+import androidx.navigation.compose.rememberNavController
+import com.example.narendranlabassignment.ui.theme.LabAssignmentTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -22,10 +26,37 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
-                    DestinationsNavHost(navGraph = NavGraphs.root)
+                    val navController = rememberNavController()
+                    Scaffold(
+                        bottomBar = { BottomNavBar(
+                            items = listOf(
+                                BottomNavItem(
+                                    name = "Home",
+                                    route = "home",
+                                    icon = Icons.Default.Home
+                                ),
+                                BottomNavItem(
+                                    name = "Lab 4",
+                                    route = "alllab",
+                                    icon = Icons.Default.List
+                                ),
+                                BottomNavItem(
+                                    name = "Profile",
+                                    route = "profile",
+                                    icon = Icons.Default.Person
+                                )
+                            ),
+                            navController = navController,
+                            onItemClick = {
+                                navController.navigate(it.route)
+                            }
+                        )}
+                    ) {
+                        Navigation(navController = navController)
+                    }
                 }
             }
         }
     }
 }
+
